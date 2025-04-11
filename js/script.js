2a0c4a93fa44eeebb5debe78b9bf826fca222dd0
Djaco <ilyas.zeynullin@gmail.com>
2025-04-11 09:55:32 +0300

    filters bag fixed and code clean up

--- js/script.js
+++ js/script.js
@@ -56,82 +56,81 @@ addClassOnClick('materials', 'materials', 'header__nav__item__active');
 
 (function () {
 
-let navBarCollection = document.getElementsByClassName('header__nav__item-box');
-for (let navBar of navBarCollection) {
+    let navBarCollection = document.getElementsByClassName('header__nav__item-box');
+    for (let navBar of navBarCollection) {
 
-    showElement = () => {
-        navBar.classList.add('box__active');
+        showElement = () => {
+            navBar.classList.add('box__active');
+        }
+
+        navBar.previousElementSibling.addEventListener('mousemove', showElement);
+        navBar.addEventListener('mousemove', showElement);
+
+        hideElement = () => {
+            navBar.classList.remove('box__active');
+        }
+
+        navBar.previousElementSibling.addEventListener('mouseleave', hideElement)
+        navBar.addEventListener('mouseleave', hideElement);
     }
-
-    navBar.previousElementSibling.addEventListener('mousemove', showElement);
-    navBar.addEventListener('mousemove', showElement);
-
-    hideElement = () => {
-        navBar.classList.remove('box__active');
-    }
-
-    navBar.previousElementSibling.addEventListener('mouseleave', hideElement)
-    navBar.addEventListener('mouseleave', hideElement);
-}
 })();
 
 
 let layoutLists = document.getElementsByClassName('layout__list__item');
 let layoutFilters = document.getElementsByClassName('layout__content__item');
 
-function hideContent(){
+function hideContent() {
+
     for (let filter of layoutFilters) {
+
         if (this.id === 'Все') {
+
             filter.style.display = 'block';
-        }
-        else if (filter.innerHTML.includes(`${this.id}`)) {
+
+        } else if (filter.firstElementChild.innerHTML.includes(`${this.id}`)) {
+
             filter.style.display = 'block';
+
         } else {
+
             filter.style.display = 'none';
         }
     }
 
 
-
     changeLayout = () => {
+
         let result = 0;
         for (let filter of layoutFilters) {
-
-            let layoutCart = filter.firstChild;
 
             if (filter.style.display === 'block' && filter.classList.contains('smartphoneLayout')) {
 
                 filter.style.gridColumn = 'span 7';
-                filter.classList.add('sevenСolumns');
-            }
 
-            else if (filter.style.display === 'block' && filter.classList.contains('tabletLayout') &&
+            } else if (filter.style.display === 'block' && filter.classList.contains('tabletLayout') &&
                 result < 2) {
 
                 filter.style.gridColumn = 'span 7';
+                filter.classList.remove('threeСolumns');
                 filter.classList.add('sevenСolumns');
                 result += 1;
-            }
 
-            else if (filter.style.display === 'block' && filter.classList.contains('tabletLayout') &&
+            } else if (filter.style.display === 'block' && filter.classList.contains('tabletLayout') &&
                 result >= 2) {
 
                 filter.style.gridColumn = 'span 3';
                 filter.classList.remove('twoСolumns');
                 filter.classList.add('threeСolumns');
 
-            }
-
-            else if (filter.style.display === 'block' && filter.classList.contains('computerLayout') &&
+            } else if (filter.style.display === 'block' && filter.classList.contains('computerLayout') &&
                 result < 2) {
 
                 filter.style.gridColumn = 'span 3';
                 filter.classList.remove('twoСolumns');
                 filter.classList.add('threeСolumns');
                 result += 1;
-            }
 
-            else if (filter.style.display === 'block' && filter.classList.contains('computerLayout') &&
+            } else if (filter.style.display === 'block' && filter.classList.contains('computerLayout') &&
                 result >= 2) {
 
                 filter.style.gridColumn = 'span 2';
